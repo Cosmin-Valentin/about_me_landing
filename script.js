750fd1f9a36cc95aaef2b85dc82f4ed14ed15952
Cosmin-Valentin <cosminv.nicola@gmail.com>
2025-02-16 14:47:18 +0200

    Project main section with scroll functionalit

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ const headerTrigger = document.querySelector('.header-trigger')
 headerTrigger.addEventListener('click', () => {
   header.classList.toggle('is-open')
 })
+
+const heading = document.querySelector('.projects .scroll-heading')
+const projectsInner = document.querySelector('.projects-inner')
+const lastFrame = document.querySelector(
+  '.projects-inner .variant:last-of-type .framer'
+)
+const threshold = lastFrame.offsetHeight
+
+window.addEventListener('scroll', () => {
+  const headingBottom = heading.getBoundingClientRect().bottom
+  const projectsBottom = projectsInner.getBoundingClientRect().bottom
+  const distance = projectsBottom - headingBottom
+
+  if (distance < threshold) {
+    heading.style.height = `${threshold + 100}px`
+  } else {
+    heading.style.height = 'auto'
+  }
+})
